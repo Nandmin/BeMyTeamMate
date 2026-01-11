c2e7f32693c10fee63873aee2758116c78023116
Nandmin <andras78.nemeth@gmail.com>
2026-01-11 23:41:00 +0100

    Deploy fix

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD0CWx35U-Ry5o57rqJ3FaMmm7SQnu5VAk',
+    authDomain: 'bemyteammate.firebaseapp.com',
+    databaseURL: 'https://bemyteammate-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'bemyteammate',
+    storageBucket: 'bemyteammate.firebasestorage.app',
+    messagingSenderId: '592557549877',
+    appId: '1:592557549877:web:4b9655a3f7af4948fcc47c',
+    measurementId: 'G-HK3YR5501H',
+    vapidKey: '5EKZ0McUTcJw8mzyn-mBof7_vOdiDjVS48m-UunqCPI', // For FCM web push (fill when available)
+  },
+  cloudflareWorkerUrl: 'https://your-worker.workers.dev/send-notification',
+};
